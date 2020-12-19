1decdf0e16a861fd15aa1346326d92d314a5fff0
Markus Leemet <markusleemet@gmail.com>
2020-12-19 23:22:36 +0200

    Front-end: create component for dates

---
+++ front-end/components/Day.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  .day-component.custom-card
+    b-img.custom-icon.mx-auto(src="~assets/icon/day.svg" v-if="day")
+    b-img.custom-icon.mx-auto(src="~assets/icon/night.svg" v-else)
+    p
+      span Phenomenon:
+      |  {{ phenomenon }}
+    p
+      span Max temperature:
+      |  {{ tempMax }}
+    p {{ description }}
+</template>
+
+<script lang="js">
+export default {
+  name: 'Day',
+  props: {
+    tempMax: {
+      type: Number,
+      default: 0
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    phenomenon: {
+      type: String,
+      default: ''
+    },
+    day: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.day-component {
+  display: flex;
+  flex-flow: column nowrap;
+  max-width: 300px;
+  height: 400px;
+  border: 1px solid black;
+
+  background: linear-gradient(0deg, rgba(255, 0, 150, 0.3), rgba(255, 0, 150, 0.3)), url("~assets/day.jpg");
+  background-size: cover;
+  margin: 1rem;
+}
+</style>
---
+++ front-end/components/LongForecast.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  #long-forecast-component.custom-section
+    b-container
+      h2.section-heading Long forecast
+      .day-forecast(v-for="forecast in weatherForecast.forecast")
+        b-button(v-b-toggle="`${forecast.date}`") {{ forecast.date }}
+        b-collapse(:id="forecast.date")
+          .collapse-container
+            day(:phenomenon="forecast.day.phenomenon" :temp-max="forecast.day.tempMax" :description="forecast.day.text" :day="true")
+            day(:phenomenon="forecast.night.phenomenon" :temp-max="forecast.night.tempMax" :description="forecast.night.text" :day="false")
+
+</template>
+
+<script lang="js">
+import Day from './Day'
+
+export default {
+  name: 'LongForecast',
+  components: { Day },
+  props: {
+    weatherForecast: {
+      type: Object,
+      default: () => {
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#long-forecast-component {
+  .day-forecast {
+    display: flex;
+    flex-flow: column nowrap;
+
+    .collapse-container {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-evenly;
+      align-items: center;
+    }
+  }
+}
+</style>
